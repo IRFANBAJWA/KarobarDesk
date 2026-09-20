751abb3e2ab0b3adcbe51fc7d679e4ea268d609b
IRFAN BAJWA <irfanbajwa1@gmail.com>
2026-09-20 19:57:37 +0500

    Add KarobarDesk currency configuration

---
+++ config/karobardesk.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | KarobarDesk Application Settings
+    |--------------------------------------------------------------------------
+    */
+
+    'currency' => env('APP_CURRENCY', 'PKR'),
+
+    'currency_symbol' => env('APP_CURRENCY_SYMBOL', 'Rs'),
+
+];
